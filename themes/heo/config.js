8c6cd1be47a4e41d596bb3e96741842efff5e895
onehpc <onehpc1@skiff.com>
2024-01-19 14:54:16 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,17 +5,17 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://c3.pw' },
+    { title: '访问文档中心获取更多帮助', url: 'https://c3.pw' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '科学计算',
+  HEO_HERO_TITLE_2: '与软件分享',
+  HEO_HERO_TITLE_3: 'C3.PW',
+  HEO_HERO_TITLE_4: '软件安装',
+  HEO_HERO_TITLE_5: '轻松定制安装',
+  HEO_HERO_TITLE_LINK: 'https://c3.pw',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
